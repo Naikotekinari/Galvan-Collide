7ab8feedef45209b6ef7fd44bf9fded83517776a
Naikotekinari <163132808+Naikotekinari@users.noreply.github.com>
2024-03-12 15:31:39 +0000

    basic setup

---
+++ charas.js
@@ -0,0 +1,8 @@
+export const charas = [
+    [
+        {name: "Zenos", priority: 1, image:"img/error.png"}
+    ],
+    [
+        {name: "Emet-Selch", priority: 2, image:"img/error.png"}
+    ]
+]
