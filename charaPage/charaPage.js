bd33fabb33bc2fb5e1276a284e2542ef39cc0186
Chloe Medder <1723063@hc.ac.je>
2024-03-25 15:48:45 +0000

    cant remember

---
+++ charaPage/charaPage.js
@@ -0,0 +1,9 @@
+window.onload = init()
+
+function init() {
+  const charaButton1 = document.getElementById("charaButton1")
+
+  charaButton1.addEventListener("click", function () {
+    console.log("Working")
+  })
+}
