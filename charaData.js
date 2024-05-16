8e087b9dfb613e81d4e9d97fc368c6ebc43252d4
Naikotekinari <163132808+Naikotekinari@users.noreply.github.com>
2024-05-16 15:56:02 +0100

    lost motivation for coding today

--- charaData.js
+++ charaData.js
@@ -1,5 +1,25 @@
 export const charaData = {
     Moegrahl: {
-        Prio: 0, img: "moe", baseHp: 240 
+        Prio: 0, 
+        img: "moe", 
+        txt: {
+            ei: {
+                name: {
+                    1: "something",
+                    2: "something more",
+                    3: "even more"
+                    }, 
+                eff: {
+                    1: "kills more",
+                    2: "heals more",
+                    3: "mmmmmmm"
+                }, 
+                st: {
+                    1: "moegrahl did some cool stuff",
+                    2: "even cooler stuff",
+                    3: "wow thats very cool"
+                }
+            }
+        } 
     }
 }
